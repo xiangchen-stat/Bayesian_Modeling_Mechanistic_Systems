0ab8d7da5954042eca9d8617fc9146bf1f6cbef0
Xiang <xchenjhu@gmail.com>
2024-12-19 05:47:45 -0800

    autoinstall packages

--- R/init_lib.R
+++ R/init_lib.R
@@ -1,19 +1,37 @@
+# List of required packages
+required_packages <- c(
+  "mniw", "LaplacesDemon", "matrixsampling", "matrixcalc", "invgamma", 
+  "matrixStats", "loo", "Rcpp", "here", "tidyverse"
+)
+
+# Function to install and load packages if not already installed
+install_and_load <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+  }
+  library(pkg, character.only = TRUE)
+}
+
+# Install and load the required packages
+lapply(required_packages, install_and_load)
+
+# library(mniw)
+# library(LaplacesDemon) # dmatrixnorm()
+# library(matrixsampling) # rinvwishart()
+# library(matrixcalc) # is.positive.definite()
+# library(invgamma) # rinvgamma
+# library(matrixStats) # logSumExp()
+# library(loo)
+# library(Rcpp)
+# library(here)
+# library(tidyverse)
+# library(here)
+# options(tidyverse.quiet = TRUE)
+# library(matrixNormal) # dmatnorm() may have underflow
+# library(microbenchmark)
 # library(MNIW)
 # library(fst)
-library(mniw)
-library(LaplacesDemon) # dmatrixnorm()
-library(matrixsampling) # rinvwishart()
-library(matrixcalc) # is.positive.definite()
-library(invgamma) # rinvgamma
-library(matrixStats) # logSumExp()
-library(loo)
-# library(microbenchmark)
-library(Rcpp)
-library(here)
-library(tidyverse)
-options(tidyverse.quiet = TRUE)
-# library(matrixNormal) # dmatnorm() may have underflow
-library(here)
+
 source(here("R/func_gen_data.R"))
 source(here("R/func_MNIW_sampler.R"))
 source(here("R/func_FF.R"))
