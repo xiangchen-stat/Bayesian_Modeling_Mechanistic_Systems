777d3cce105cb5bb5287030894fc72efa2289538
Xiang Chen <74457944+xiangchen-stat@users.noreply.github.com>
2024-12-19 09:05:58 -0800

    rollback init.R

--- R/init_lib.R
+++ R/init_lib.R
@@ -1,32 +1,32 @@
-# List of required packages
-required_packages <- c(
-  "mniw", "LaplacesDemon", "matrixsampling", "matrixcalc", "invgamma", 
-  "matrixStats", "loo", "Rcpp", "here", "tidyverse"
-)
-
-# Function to install and load packages if not already installed
-install_and_load <- function(pkg) {
-  if (!require(pkg, character.only = T)) {
-    install.packages(pkg)
-  }
-  library(pkg, character.only = TRUE)
-}
-
-for(pkg in required_packages){
-  install_and_load(pkg)
-}
-# library(mniw)
-# library(LaplacesDemon) # dmatrixnorm()
-# library(matrixsampling) # rinvwishart()
-# library(matrixcalc) # is.positive.definite()
-# library(invgamma) # rinvgamma
-# library(matrixStats) # logSumExp()
-# library(loo)
-# library(Rcpp)
-# library(here)
-# library(tidyverse)
-# library(here)
-# options(tidyverse.quiet = TRUE)
+# # List of required packages
+# required_packages <- c(
+#   "mniw", "LaplacesDemon", "matrixsampling", "matrixcalc", "invgamma", 
+#   "matrixStats", "loo", "Rcpp", "here", "tidyverse"
+# )
+# 
+# # Function to install and load packages if not already installed
+# install_and_load <- function(pkg) {
+#   if (!require(pkg, character.only = T)) {
+#     install.packages(pkg)
+#   }
+#   library(pkg, character.only = TRUE)
+# }
+# 
+# for(pkg in required_packages){
+#   install_and_load(pkg)
+# }
+library(mniw)
+library(LaplacesDemon) # dmatrixnorm()
+library(matrixsampling) # rinvwishart()
+library(matrixcalc) # is.positive.definite()
+library(invgamma) # rinvgamma
+library(matrixStats) # logSumExp()
+library(loo)
+library(Rcpp)
+library(here)
+library(tidyverse)
+library(here)
+options(tidyverse.quiet = TRUE)
 # library(matrixNormal) # dmatnorm() may have underflow
 # library(microbenchmark)
 # library(MNIW)
